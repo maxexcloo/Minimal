005cee2d9aec490b02230b91ef57df9d5fd919cc
maxexcloo <max@excloo.com>
2011-12-03 16:57:12 +1100

    Changed Tense In Documentation

--- minimal.sh
+++ minimal.sh
@@ -5,14 +5,14 @@ cd $(dirname $0)
 ## Variables ##
 ###############
 
-# Disables BASH History For Session
+# Disable BASH History For Session
 unset HISTFILE
 
 #############################
 ## Configuration Functions ##
 #############################
 
-# Runs Through Configuration Functions
+# Run Through Configuration Functions
 function configure_basic {
 	# Configure Defaults
 	configure_defaults
@@ -23,54 +23,54 @@ function configure_basic {
 	# Ask If BASH History Should Be Disabled
 	echo -n "Do you wish to disable BASH history? (Y/n): "
 	read -e OPTION_HISTORY
-	# Checks User Input
+	# Check User Input
 	if [ "$OPTION_HISTORY" != "n" ]; then
-		# Executes Function
+		# Execute Function
 		configure_history
 	fi
 
 	# Ask If Logging Should Be Simplified
 	echo -n "Simplify logging configuration? (Y/n): "
 	read -e OPTION_LOGGING
-	# Checks User Input
+	# Check User Input
 	if [ "$OPTION_LOGGING" != "n" ]; then
-		# Executes Function
+		# Execute Function
 		configure_logging
 	fi
 
 	# Ask If SSH Port Should Be Changed
 	echo -n "Do you wish to run SSH on different ports? (y/N): "
 	read -e OPTION_SSHPORT
-	# Checks User Input
+	# Check User Input
 	if [ "$OPTION_SSHPORT" == "y" ]; then
-		# Executes Function
+		# Execute Function
 		configure_sshport
 	fi
 
 	# Ask If Root SSH Should Be Disabled
 	echo -n "Do you wish to disable root SSH logins? Keep enabled if you don't plan on making any users! (Y/n): "
 	read -e OPTION_SSHROOT
-	# Checks User Input
+	# Check User Input
 	if [ "$OPTION_SSHROOT" != "n" ]; then
-		# Executes Function
+		# Execute Function
 		configure_sshroot
 	fi
 
 	# Ask If Time Zone Should Be Set
 	echo -n "Do you wish to set the timezone? (Y/n): "
 	read -e OPTION_TZ
-	# Checks User Input
+	# Check User Input
 	if [ "$OPTION_TZ" != "n" ]; then
-		# Executes Function
+		# Execute Function
 		configure_timezone
 	fi
 
 	# Ask If User Should Be Made
 	echo -n "Do you wish to create a user account? (Y/n): "
 	read -e OPTION_USER
-	# Checks User Input
+	# Check User Input
 	if [ "$OPTION_USER" != "n" ]; then
-		# Executes Function
+		# Execute Function
 		configure_user
 	fi
 
@@ -81,7 +81,7 @@ function configure_basic {
 	configure_final
 }
 
-# Cleans Dotfiles
+# Clean Dotfiles
 function configure_defaults {
 	echo \>\> Configuring: Defaults
 	# Remove Home Dotfiles
@@ -96,7 +96,7 @@ function configure_defaults {
 	echo -e "\numask o=" >> /etc/skel/.bashrc
 }
 
-# Cleans Home
+# Clean Home
 function configure_final {
 	echo \>\> Configuring: Finalizing
 	# Remove All Home Files
@@ -106,72 +106,72 @@ function configure_final {
 # Clean Getty
 function configure_getty {
 	echo \>\> Configuring: Gettys
-	# Removes Unneeded Getty Instances
+	# Remove Unneeded Getty Instances
 	sed -e 's/\(^[2-6].*getty.*\)/#\1/' -i /etc/inittab
 }
 
-# Disables BASH History
+# Disable BASH History
 function configure_history {
 	echo \>\> Configuring: BASH History
-	# Disables System BASH History
+	# Disable System BASH History
 	echo -e "\nunset HISTFILE" >> /etc/profile
 }
 
-# Simplifies Logging
+# Simplify Logging
 function configure_logging {
 	echo \>\> Configuring: Simplified Logging
-	# Stops Logging Daemon
+	# Stop Logging Daemon
 	/etc/init.d/inetutils-syslogd stop
-	# Removes Log Files
+	# Remove Log Files
 	rm /var/log/* /var/log/*/*
 	rm -rf /var/log/news
-	# Creates New Log Files
+	# Create New Log Files
 	touch /var/log/{auth,daemon,kernel,mail,messages}
-	# Copies Simplified Logging Configuration
+	# Copy Simplified Logging Configuration
 	cp settings/syslog /etc/syslog.conf
-	# Copies Simplified Log Rotation Configuration
+	# Copy Simplified Log Rotation Configuration
 	cp settings/logrotate /etc/logrotate.d/inetutils-syslogd
-	# Starts Logging Daemon
+	# Start Logging Daemon
 	/etc/init.d/inetutils-syslogd start
 }
 
-# Changes SSH Port To User Specification
+# Add Additional SSH Port
 function configure_sshport {
 	echo \>\> Configuring: Changing SSH Ports
-	# Takes User Input
+	# Take User Input
 	echo -n "Please enter an additional SSH Port: "
 	read -e SSHPORT
-	# Adds Extra SSH Port To OpenSSH
+	# Add Extra SSH Port To OpenSSH
 	sed -i 's/#Port/Port '$SSHPORT'/g' /etc/ssh/sshd_config
-	# Adds Extra SSH Port To Dropbear
+	# Add Extra SSH Port To Dropbear
 	sed -i 's/DROPBEAR_EXTRA_ARGS="-w/DROPBEAR_EXTRA_ARGS="-w -p '$SSHPORT'/g' /etc/default/dropbear
 }
 
-# Enables Root SSH Login
+# Disable Root SSH Login
 function configure_sshroot {
 	echo \>\> Configuring: Disabling Root SSH Login
-	# Disables Root SSH Login For OpenSSH
+	# Disable Root SSH Login For OpenSSH
 	sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-	# Disables Root SSH Login For Dropbear
+	# Disable Root SSH Login For Dropbear
 	sed -i 's/DROPBEAR_EXTRA_ARGS="/DROPBEAR_EXTRA_ARGS="-w/g' /etc/default/dropbear
 }
 
-# Sets Time Zone
+# Set Time Zone
 function configure_timezone {
 	echo \>\> Configuring: Time Zone
-	# Configures Time Zone
+	# Configure Time Zone
 	dpkg-reconfigure tzdata
 }
 
-# Adds User Account
+# Add User Account
 function configure_user {
 	echo \>\> Configuring: User Account
-	# Takes User Input
+	# Take User Input
 	echo -n "Please enter a user name: "
 	read -e USERNAME
-	# Adds User Based On Input
+	# Add User Based On Input
 	useradd -m -s /bin/bash $USERNAME
-	# Sets Password For Newly Added User
+	# Set Password For Newly Added User
 	passwd $USERNAME
 }
 
@@ -179,7 +179,7 @@ function configure_user {
 ## Installation Functions ##
 ############################
 
-# Executes Install Functions
+# Execute Install Functions
 function install_basic {
 	packages_update
 	packages_purge
@@ -188,43 +188,43 @@ function install_basic {
 	packages_purge
 }
 
-# Installs Lightweight Dropbear SSH Server & OpenSSH For SFTP Support
+# Install Lightweight Dropbear SSH Server & OpenSSH For SFTP Support
 function install_dropbear {
 	echo \>\> Configuring Dropbear
-	# Installs Dropbear
+	# Install Dropbear
 	apt-get install dropbear
-	# Updates Configuration Files
+	# Update Configuration Files
 	cp settings/dropbear /etc/default/dropbear
-	# Installs OpenSSH For SFTP Support
+	# Install OpenSSH For SFTP Support
 	install_ssh
-	# Removes OpenSSH Daemon
+	# Remove OpenSSH Daemon
 	update-rc.d -f ssh remove
-	# Cleans Package List
+	# Clean Package List
 	packages_purge
 }
 
-# Installs Extra Packages Defined In List
+# Install Extra Packages Defined In List
 function install_extra {
-	# Loops Through Package List
+	# Loop Through Package List
 	while read package; do
-		# Installs Currently Selected Package
+		# Install Currently Selected Package
 		apt-get -q -y install $package
 	done < extra
-	# Cleans Cached Packages
+	# Clean Cached Packages
 	apt-get clean
 }
 
-# Installs OpenSSH And Sets Configuration
+# Install OpenSSH & Sets Configuration
 function install_ssh {
 	echo \>\> Configuring SSH
-	# Installs OpenSSH
+	# Install OpenSSH
 	apt-get install openssh-server
-	# Copies SSH Configuration Files
+	# Copy SSH Configuration Files
 	cp settings/sshd /etc/ssh/sshd_config
 	cp settings/ssh /etc/ssh/ssh_config
-	# Restarts OpenSSH Daemon
+	# Restart OpenSSH Daemon
 	/etc/init.d/ssh restart
-	# Cleans Package List
+	# Clean Package List
 	packages_purge
 }
 
@@ -232,14 +232,14 @@ function install_ssh {
 ## Package Functions ##
 #######################
 
-# Uses DPKG To Remove Packages
+# Use DPKG To Remove Packages
 function packages_clean {
 	echo \>\> Cleaning Packages
 	# Clear DPKG Package Selections
 	dpkg --clear-selections
 	# Set Package Selections
 	dpkg --set-selections < lists/temp
-	# Get Selections And Set To Purge
+	# Get Selections & Set To Purge
 	dpkg --get-selections | sed -e 's/deinstall/purge/' > /tmp/dpkg
 	# Set Package Selections
 	dpkg --set-selections < /tmp/dpkg
@@ -249,7 +249,7 @@ function packages_clean {
 	apt-get upgrade
 }
 
-# Creates Package List
+# Create Package List
 function packages_create {
 	echo \>\> Creating Package List
 	# Copy Base Package List
@@ -290,23 +290,23 @@ function packages_create {
 	sort -o lists/temp lists/temp
 }
 
-# Purges APT Package Lists
+# Purge APT Package Lists
 function packages_purge {
 	echo \>\> Cleaning Package States
 	# Empty Package List Files
 	echo -n > /var/lib/apt/extended_states
-	# Cleans Cached Packages
+	# Clean Cached Packages
 	apt-get clean
 }
 
-# Updates Sources List & APT
+# Update Sources List & APT
 function packages_update {
 	echo \>\> Setting Up APT Sources
-	# Copies Sources
+	# Copy Sources
 	cp settings/sources /etc/apt/sources.list
-	# Adds DotDeb Source Key
+	# Add DotDeb Source Key
 	wget http://www.dotdeb.org/dotdeb.gpg -qO - | apt-key add -
-	# Updates Package Lists
+	# Update Package Lists
 	apt-get update
 }
 
@@ -315,25 +315,25 @@ function packages_update {
 #################
 
 case "$1" in
-	# Minimises System And Installs Dropbear
+	# Minimise System & Install Dropbear
 	dropbear)
 		install_basic
 		install_dropbear
 	;;
-	# Installs Extra Packages
+	# Install Extra Packages
 	extra)
 		install_extra
 	;;
-	# Configures Install
+	# Configure Install
 	configure)
 		configure_basic
 	;;
-	# Minimises System And Installs OpenSSH
+	# Minimise System & Install OpenSSH
 	ssh)
 		install_basic
 		install_ssh
 	;;
-	# Shows Help
+	# Show Help
 	*)
 		echo \>\> You must run this script with options. They are outlined below:
 		echo For a minimal Dropbear based install: bash minimal.sh dropbear
